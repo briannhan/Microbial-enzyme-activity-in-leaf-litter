b49aed2bc72b7216ba1c43735e28d699d7e41202
Nhan Chung <brian166chung@gmail.com>
2020-11-27 03:06:01 -0800

    Began pre-processing T0 Black
    I only began this process; I haven't completed it. T0 Black will be pre-processed before its enzyme activities will be calculated. This pre-processing includes (1) processing long sample names in the T0 Black file; (2) Checking for matches in sample names between the dry weights dataframe and T0 Black; (3) rearranging buffer plate data; and (4) adding dry assay mass to T0 Black.
    
    I will process T0 Black until its ready for enzyme activity calculations and then calculate the enzyme activity for T0 Black. After doing all of this for T0 Black, I will also do the same for T0 Clear: pre-process T0 Clear using the tasks mentioned above and calculate oxidative enzyme activity of T0 Clear

--- calculateEnzymeActivity.py
+++ calculateEnzymeActivity.py
@@ -4,7 +4,16 @@ Created on Wed Nov 25 23:24:06 2020
 
 @author: Brian Chung
 The purpose of this script is to calculate the enzyme activity of the litter
-from Loma Ridge. This script processes enzyme activity & litter dry weight data
+from Loma Ridge. This script processes enzyme activity & litter dry weight
+data.
+
+I delineate this script into sections using the following characters: # %%
+These sections are visualized in the Spyder IDE of the Anaconda distribution
+of Python. Each section has a particular purpose, and several tasks are
+numbered to accomplish that purpose. The number in the tasks doesn't reflect
+the order at which they are done. For examples, if I list tasks (1), (2), and
+(3), I may complete task (3) before task (2). However, all the tasks in each
+section will be completed to carry out the purpose of that section.
 """
 # %%
 # Importing necessary libraries
@@ -28,7 +37,7 @@ dryMassFiles = os.listdir(dryMassFolderPath)
 # Processing dry weight data before using it for enzyme activity calculations
 # Processing includes (1) renaming columns; (2) Filling in time points;
 # (3) Adding columns for wet and dry assay mass and proportion of leaf litter
-# as dry mass; (4) Calculating proportion dry assay mass; and (5) isolating
+# as dry mass and calculating proportion dry assay mass; and (4) isolating
 # timepoints T0, T3, and T5 (T6 will be isolated from a different spreadsheet)
 
 # The following line makes pandas print all columns
@@ -39,13 +48,11 @@ dryMassFilePath = dryMassFolderPath/dryMassFiles[0]
 dryDF = pd.read_excel(dryMassFilePath)
 
 # (1) Renaming columns
-oldColumns = dryDF.columns.tolist()
-newColumns = ["Envelope mass (g)", "Envelope + wet (g)",
-              "Envelope + dry (g)", "Wet litter mass (g)",
-              "Dry litter mass (g)"]
-newColumnsDict = {oldColumns[2]: newColumns[0], oldColumns[3]: newColumns[1],
-                  oldColumns[4]: newColumns[2], oldColumns[5]: newColumns[3],
-                  oldColumns[6]: newColumns[4]}
+oldCols = dryDF.columns.tolist()
+newCols = ["Envelope mass (g)", "Envelope + wet (g)", "Envelope + dry (g)",
+           "Wet litter mass (g)", "Dry litter mass (g)"]
+
+newColumnsDict = {oldCols[n + 2]: newCols[n] for n in range(len(newCols))}
 dryDF = dryDF.rename(columns=newColumnsDict)
 
 
@@ -56,18 +63,81 @@ for index, row in dryDF.iterrows():
         previousRow = dryDF.iloc[index - 1]
         dryDF.loc[index, "Time"] = previousRow["Time"]
 
-# (3) & (4) Adding columns & calculating dry mass
+# (3) Adding columns & calculating dry assay mass
 dryDF["Wet assay (g)"] = 0.4
 dryDF["Dry proportion (%)"] = 100*(dryDF["Dry litter mass (g)"] /
                                    dryDF["Wet litter mass (g)"])
 dryDF["Dry assay (g)"] = dryDF["Wet assay (g)"]*dryDF["Dry proportion (%)"]/100
 
-# (5) Isolating timepoints T0, T3, T5
+# (4) Isolating timepoints T0, T3, T5
 timepoints = dryDF.groupby("Time")["Time"].count().index.tolist()
 timepoints = [timepoints[0], timepoints[3], timepoints[5]]
 T0_3_5Bool = dryDF["Time"].isin(timepoints)
 dryDF = dryDF[T0_3_5Bool]
+# %%
+# I'm going to test with a single enzyme file first as a proof of concept. In
+# other words, I'm going to do some pre-processing for a single enzyme file
+# in this section before going on to calculate enzyme activities in the
+# following sections.
+# The tasks for this section are: 
+# (1) Process long sample names in the enzyme file;
+# (2) Check to see if the names in the enzyme files match the names in the dry
+# weights files; if there isn't a match, then some samples are missing or some
+# samples are redone; (3) rearrange the buffer plate data in a way to ease
+# calculations (4) add dry assay mass to the data and
 
-# I'm going to test with a single enzyme file & dry weights file
-# Reading in 1 enzyme file and the dry weights excel that contains dry weights
-# for all time points
+# Obtaining a list of samples to check if plate data contain samples
+samples = dryDF.groupby("ID")["ID"].count().index.tolist()
+
+# Reading in T0 black plates
+ogEnzymeCols = ["Well", "Long sample name", "Fluorescence"]
+T0BlackPath = enzymeFolderPath/enzymeFiles[0]
+T0Black = pd.read_csv(T0BlackPath, '\t', header=None, names=ogEnzymeCols)
+
+# (1) Processing long sample names. The long sample names have a lot of junk in
+# them so I'm processing them to keep (1) the assay date and (2) information
+# for whether the plate is a buffer of a sample plate. If the plate is a sample
+# plate, then it would contain the sample ID
+T0Black["Long sample name"] = T0Black["Long sample name"].str.split("_")
+for index, row in T0Black.iterrows():
+    longSampleName = row["Long sample name"]
+    T0Black.loc[index, "Assay date"] = longSampleName[0]
+    if "B" in longSampleName:
+        T0Black.loc[index, "ID"] = "B"
+    elif "X" in longSampleName[2]:
+        T0Black.loc[index, "ID"] = longSampleName[2]
+T0Black = T0Black.drop(labels="Long sample name", axis=1)
+
+# (2) Checking to see if enzyme activity data contains all samples
+T0BlackCounts = T0Black.groupby("ID")["ID"].count()/96
+# If any of the counts of the samples return 2 or greater, then that means
+# that that sample was assayed at least twice. But what if one sample has
+# a different name than what the name should be?
+T0BlackSamples = T0BlackCounts.index.tolist()
+samplesNotInT0Black = []
+for sample in samples:
+    if sample not in T0BlackSamples:
+        samplesNotInT0Black.append(sample)
+# if samplesNotInT0Black is empty, that means that all the sample names in
+# the dry weights data frame match the sample names in the enzyme data, and so
+# no samples are mis-named or missing
+
+
+# (4) Adding dry assay mass to enzyme data frame
+T0Bool = dryDF["Time"].isin(["T0 (November 30, 2017)"])
+dryDF_T0 = dryDF[T0Bool]
+oldCols = dryDF_T0.columns.tolist()
+colsToDrop = [oldCols[0]]
+colsToDrop.extend(oldCols[2:-1])
+dryDF_T0 = dryDF_T0.drop(labels=colsToDrop, axis=1)
+T0Black = pd.merge(T0Black, dryDF_T0, how="left", on="ID")
+# be sure to read up on pandas documentation to figure out how the merge
+# method works. Go to section 2.7.2 of the guide on pandas version 1.1.4, page
+# 448 (462 on the online pdf reader)
+
+
+# (3) Rearranging buffer plate data. I intend that, for each sample, the
+# absorbance in a particular well will also correspond with the absorbance
+# reading in the buffer plate at that particular well. There will be 2 columns
+# one column to hold absorbance for a particular well of the sample while the
+# second column holds the absorbance for the same well for the buffer plate
