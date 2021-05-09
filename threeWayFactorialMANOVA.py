3e52cf8e3e172bac8693a0a457b435b2cc6317c7
Nhan Chung <brian166chung@gmail.com>
2021-05-09 14:59:20 -0700

    Tested some syntax in running ANOVAs
    I was trying to see whether to use the "Sum" thing in the formulas for my ANOVAs, and whether to use the C() thing as well as the differences between asterisks and colons. It seems like if I'm doing a type 3 ANOVA, I would need to incorporate the "Sum" thing, and in order to use it, I need to use the "C()" thing as well. So, to do a type 3 ANOVA, I would need to use both "Sum" and "C()". If I'm only doing a type 2 ANOVA, then I only need to use the "C()" thing for a factor whose values are numerical when the factor is actually categorical and the numbers stand for discrete categories.
    
    I also moved the AG Vmax ANOVA syntax below the MANOVA chunk.

--- threeWayFactorialMANOVA.py
+++ threeWayFactorialMANOVA.py
@@ -29,45 +29,12 @@ dataDirsFiles = os.listdir(dataFolder)
 paramsPath = dataFolder/'Parameters - log 10 transformed.xlsx'
 parameters = pd.read_excel(paramsPath)
 # %%
-# Purpose: Carry out three-way ANOVA on AG's Vmax as a proof of concept to code
-# ANOVAs for the remaining 7 enzymes. This process will be run iteratively if
-# interactions between at least 2 independent variables (vegetation, time
-# points, and precipitation) are non-significant, and each non-significant
-# interaction will be broken down into simpler interactions and removed until
-# the significant simpler interactions remain.
-
-# (1) Subsetting out AG's Vmax
-conditions = (parameters.Enzyme == "AG") & (parameters.Parameter == "Vmax")
-VmaxAG = parameters[conditions]
-
-# (2) Initial linear regression model and ANOVA attempt
-initFormula = "value ~ C(timePoint, Sum) + C(Vegetation, Sum) + C(Precip, Sum) + C(timePoint, Sum)*C(Vegetation, Sum)*C(Precip, Sum)"
-initModel = ols(formula=initFormula, data=VmaxAG).fit()
-initialResults = anova_lm(initModel, typ=3)
-'''So the three-way interaction isn't significant, time to split it up into
-the 2-way interactions'''
-
-# (3) Splitting up three-way interaction
-sepInteractionsForm = "value ~ C(timePoint, Sum) + C(Vegetation, Sum) + C(Precip, Sum) + C(timePoint, Sum):C(Vegetation, Sum) + C(timePoint, Sum):C(Precip, Sum) + C(Vegetation, Sum):C(Precip, Sum)"
-sepInteractionsModel = ols(formula=sepInteractionsForm, data=VmaxAG).fit()
-results2 = anova_lm(sepInteractionsModel, typ=3)
-'''So there are no interaction effects between any of the 3 independent
-variables on AG Vmax parameter values, even when they're all separated into
-2-way interactions. So, let's drop all the non-significant interactions'''
-
-# (4) ANOVA with no interactions
-noInteractionsForm = "value ~ C(timePoint, Sum) + C(Vegetation, Sum) + C(Precip, Sum)"
-noInteractionsModel = ols(formula=noInteractionsForm, data=VmaxAG).fit()
-results3 = anova_lm(noInteractionsModel, typ=3)
-'''So the main effects alone (time point, vegetation, and precip) don't have
-any effect on AG either.'''
-
+# Purpose: Performing a factorial MANOVA with the factors being time point,
+# vegetation, and precipitation. This factorial MANOVA is used as exploratory
+# data analysis to see which relationships are potentially significant.
 '''Ok so just talked with Steve and he recommended the following tests in the
 following order: factorial MANOVA for all enzymes, followed by factorial
 ANOVAs & Tukey for individual enzymes. So Imma do that instead'''
-# %%
-# Purpose: Performing a factorial MANOVA with the factors being time point,
-# vegetation, and precipitation
 
 # (1) Convert the parameters dataframe into a MultiIndex
 parameters = parameters.drop(columns=["Transformation"])
@@ -117,3 +84,39 @@ paramsWide = pd.merge(paramsHydro, paramsOxiPivot, on=["timePoint", "ID",
 manovaFormula = "AG_Vmax + AG_Km + AP_Vmax + AP_Km + BG_Vmax + BG_Km + BX_Vmax + BX_Km + CBH_Vmax + CBH_Km + LAP_Vmax + LAP_Km + NAG_Vmax + NAG_Km + PPO_Vmax_1 + PPO_Vmax_2 + PPO_Km_1 + PPO_Km_2 ~ C(timePoint, Sum)*C(Vegetation, Sum)*C(Precip, Sum)"
 manovaModel = MANOVA.from_formula(formula=manovaFormula, data=paramsWide)
 manovaResults = manovaModel.mv_test()
+# %%
+# Purpose: Carry out three-way ANOVA on AG's Vmax as a proof of concept to code
+# ANOVAs for the remaining 7 enzymes. This process will be run iteratively if
+# interactions between at least 2 independent variables (vegetation, time
+# points, and precipitation) are non-significant, and each non-significant
+# interaction will be broken down into simpler interactions and removed until
+# the significant simpler interactions remain.
+
+# (1) Subsetting out AG's Vmax
+conditions = (parameters.Enzyme == "AG") & (parameters.Parameter == "Vmax")
+VmaxAG = parameters[conditions]
+
+# (2) Initial linear regression model and ANOVA attempt
+initFormula = "value ~ C(timePoint, Sum)*C(Vegetation, Sum)*C(Precip, Sum)"
+initModel = ols(formula=initFormula, data=VmaxAG).fit()
+initialResults = anova_lm(initModel, typ=3)
+'''So the three-way interaction isn't significant, time to split it up into
+the 2-way interactions'''
+
+# (3) Splitting up three-way interaction
+timeXveg = "C(timePoint, Sum)*C(Vegetation, Sum)"
+timeXppt = "C(timePoint, Sum)*C(Precip, Sum)"
+vegXppt = "C(Vegetation, Sum)*C(Precip, Sum)"
+sepInteracForm = "value ~ {0} + {1} + {2}".format(timeXveg, timeXppt, vegXppt)
+sepInteracModel = ols(formula=sepInteracForm, data=VmaxAG).fit()
+results2 = anova_lm(sepInteracModel, typ=3)
+'''So there are no interaction effects between any of the 3 independent
+variables on AG Vmax parameter values, even when they're all separated into
+2-way interactions. So, let's drop all the non-significant interactions'''
+
+# (4) ANOVA with no interactions
+noInteracForm = "value ~ C(timePoint) + Vegetation + Precip"
+noInteracModel = ols(formula=noInteracForm, data=VmaxAG).fit()
+results3 = anova_lm(noInteracModel, typ=2)
+'''So the main effects alone (time point, vegetation, and precip) don't have
+any effect on AG either.'''
